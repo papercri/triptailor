42edda297a1ca1a17151e104f13cab3facef9d0c
Your NameCristiana <papercri@gmail.com>
2025-06-26 21:03:22 +0200

    fixes for vercel

--- src/app/destination/[place]/page.tsx
+++ src/app/destination/[place]/page.tsx
@@ -1,12 +1,17 @@
 import { notFound } from 'next/navigation';
 import DestinationClient from './DestinationClient';
+import { FC } from "react";
 
-type Props = {
-  params: { place: string };
-};
+  type Props = {
+    params: { place: string };
+  };
+const DestinationPage: FC<Props> = ({ params }) => {
+  const { place } = params;
 
-export default function DestinationPage({ params }: Props) {
-  if (!params.place) notFound();
+   if (!place) notFound();
 
   return <DestinationClient place={decodeURIComponent(params.place)} />;
-}
+};
+
+export default DestinationPage;
+
