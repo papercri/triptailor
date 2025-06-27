bfe8c87cd86b3ad17e7618a2737bc6d51e82f3c8
Your NameCristiana <papercri@gmail.com>
2025-06-27 12:47:07 +0200

    removed error3

--- src/app/destination/[place]/DestinationClient.tsx
+++ src/app/destination/[place]/DestinationClient.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import '@/styles/destination.scss';
-import React, { Suspense } from 'react';
+//import React, { Suspense } from 'react';
 import Header from '@/components/layout/header/Header';
 import Footer from '@/components/layout/footer/Footer';
 import DestinationHero from '@/components/destination/hero/DestinationHero';
