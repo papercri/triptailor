7e0d6973569cdb7d1491dde1646ffb8b7aff9148
Your NameCristiana <papercri@gmail.com>
2025-06-28 10:13:58 +0200

    test api build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
-  
-
-  
 };
 
 export default nextConfig;
