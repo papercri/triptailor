63459a263e95dbf0990cc27bce808516488ac93c
Your NameCristiana <papercri@gmail.com>
2025-06-28 10:02:41 +0200

    test api

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   trailingSlash: false,
-  experimental: {
-  },
-  async headers() {
-    return [
-      {
-        source: "/destination/:path*",
-        headers: [
-          {
-            key: "x-custom-header",
-            value: "destination-route",
-          },
-        ],
-      },
-    ]
-  },
+
   
 };
 
